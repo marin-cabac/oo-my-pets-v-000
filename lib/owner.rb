ca8a5a5bdb316989372c904df8aaff38c1dca6d1
Marin Cabac <marin.cabac@gmail.com>
2018-03-07 12:12:17 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -47,7 +47,7 @@ class Owner
   end
   def list_pets
      
-    "I have #{@pets[fishes].length} fish, #{@pets[dogs].length} dog(s), and #{@pets[cats].length} cat(s)."
+    "I have #{@pets[fishes].size} fish, #{@pets[dogs].size} dog(s), and #{@pets[cats].size} cat(s)."
   end
 
 
